dc6184c262bb5e4d62c2dfcd3411e6fc4c15d014
Vinh Phan <pvphan90@gmail.com>
2022-12-22 16:19:20 -0800

    visualzing 2 spheres

--- main.cc
+++ main.cc
@@ -5,30 +5,13 @@
 #include "hittablelist.h"
 #include "sphere.h"
 
-double hitSphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().lengthSquared();
-    auto halfB = dot(oc, r.direction());
-    auto c = oc.lengthSquared() - radius*radius;
-    auto discriminant = halfB*halfB - a*c;
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-halfB - sqrt(discriminant)) / a;
-    }
-}
-
-color rayColor(const ray& r) {
-    //point3 sphereCenter = point3(0.5, 0.7, -1);
-    point3 sphereCenter = point3(0, 0, -1);
-    double sphereRadius = 0.5;
-    auto t = hitSphere(sphereCenter, sphereRadius, r);
-    if (t > 0.0) {
-        vec3 N = unitVector(r.at(t) - vec3(0, 0, -1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color rayColor(const ray& r, const Hittable& world) {
+    HitRecord rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
     vec3 unitDirection = unitVector(r.direction());
-    t = 0.5 * (unitDirection.y() + 1.0);
+    auto t = 0.5 * (unitDirection.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -61,7 +44,7 @@ int main() {
             auto u = double(i) / (imageWidth-1);
             auto v = double(j) / (imageHeight-1);
             ray r(origin, lowerLeftCorner + u*horizontal + v*vertical - origin);
-            color pixelColor = rayColor(r);
+            color pixelColor = rayColor(r, world);
             writeColor(std::cout, pixelColor);
         }
     }
