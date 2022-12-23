04ada16a96fe27d05a99b1ab11a985900bd97547
Vinh Phan <pvphan90@gmail.com>
2022-12-22 16:26:48 -0800

    added camera.h

--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -14,6 +15,16 @@ const double pi = 3.1415926535897932385;
 
 inline double degreesToRadians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double randomDouble() {
+    // Returns a random real in [0, 1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double randomDouble(double min, double max) {
+    // Returns a random real in [min, max)
+    return min + (max-min)*randomDouble();
 }
 
 #include "ray.h"
